456c3a73b5319ec776f599ee13233b523edd3996
adildrx <adil.belhamida@etu.univ-orleans.fr>
2026-05-28 12:07:55 +0200

    Modif GenerationGuideeTest.java implementation GenerationAleatoireTest.java

--- livrejeu/src/test/java/com/livrejeu/generation/GenerationGuideeTest.java
+++ livrejeu/src/test/java/com/livrejeu/generation/GenerationGuideeTest.java
@@ -15,8 +15,8 @@ public class GenerationGuideeTest {
         IGenerateur generateur = new GenerateurV2();
         LivreJeu livre = generateur.generer(8, 1, 8, new HashMap<>());
         
-        assertNotNull(livre, "Le livre généré par la génération guidée ne doit pas être null.");
-        assertEquals(8, livre.getNbPages(), "Le nombre de pages générées doit être respecté.");
+        assertNotNull(livre);
+        assertEquals(8, livre.getNbPages());
         assertNotNull(livre.getPageDepart());
         assertNotNull(livre.getPageSortie());
     }
@@ -26,10 +26,10 @@ public class GenerationGuideeTest {
         IGenerateur generateur = new GenerateurV2();
         LivreJeu livre = generateur.generer(15, 1, 15, new HashMap<>());
         
-        AlgorithmeBFS algo = new AlgorithmeBFS();
+        AlgorithmeBFS algo = new AlgorithmeBFS(); 
         Solution solution = algo.resoudre(livre);
         
-        assertNotNull(solution, "La génération guidée doit toujours garantir l'existence d'une solution.");
-        assertTrue(solution.estValide(livre), "La solution trouvée doit être un chemin valide selon les règles du graphe.");
+        assertNotNull(solution);
+        assertTrue(solution.estValide(livre));
     }
 }
