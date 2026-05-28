120af04c0160df303f58200d5eb5886b0dd53f4c
adildrx <adil.belhamida@etu.univ-orleans.fr>
2026-05-28 11:33:41 +0200

    Modif import identation GenerateurV2.java

--- livrejeu/src/main/java/com/livrejeu/generation/GenerateurV2.java
+++ livrejeu/src/main/java/com/livrejeu/generation/GenerateurV2.java
@@ -1,12 +1,11 @@
 package com.livrejeu.generation;
-
 import com.livrejeu.LivreJeu;
 import com.livrejeu.Objet;
-import com.livrejeu.Solution;
 import java.util.Map;
 import java.util.Set;
 
 public class GenerateurV2 implements IGenerateur {
+    
     @Override
     public LivreJeu generer(int nbPages, int idDepart, int idSortie, Map<Integer, Set<Objet>> objets) {
         // TODO: Logique de génération plus complexe/optimisée
